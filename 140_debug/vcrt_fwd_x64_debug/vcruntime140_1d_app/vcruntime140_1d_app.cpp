04ef15701a0811e2f0c3bce8fd57d2519e358909
Manodasan Wignarajah <mawign@microsoft.com>
2019-06-11 00:14:50 -0700

    Adding forwarders for vcruntime140_1d

---
+++ 140_debug/vcrt_fwd_x64_debug/vcruntime140_1d_app/vcruntime140_1d_app.cpp
@@ -0,0 +1,1 @@
+#pragma comment(linker, "/export:__CxxFrameHandler4=vcruntime140_1d.__CxxFrameHandler4")
---
+++ 140_release/vcrt_fwd_x64_release/vcruntime140_1_app/vcruntime140_1_app.cpp
@@ -0,0 +1,1 @@
+#pragma comment(linker, "/export:__CxxFrameHandler4=vcruntime140_1.__CxxFrameHandler4")
