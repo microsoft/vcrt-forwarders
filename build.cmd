cd92001d87d13b35f398181d52300b939ef759d8
David Fields (ANALOG) <dfields@microsoft.com>
2019-09-03 12:02:07 -0700

    Added support for NuGet for Unity.

---
+++ build.cmd
@@ -0,0 +1,18 @@
+@echo off
+pushd %~dp0
+
+set STAGING_ROOT=staging
+
+for %%c in (debug release) do (
+	for %%p in (x86 x64 arm64) do (
+		pushd 140_%%c\vcrt_fwd_%%p_%%c
+		msbuild
+		for /r %%d in (*.dll) do xcopy /y "%%~d" "%~dp0\runtimes\win10-%%p\native\%%c\"
+		popd
+	)
+)
+
+where nuget.exe >nul 2>&1 || echo Couldn't find nuget.exe! && goto :eof
+nuget pack vcrt-forwarders.nuspec
+
+popd
