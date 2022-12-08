5d7e06ec3b1d16777ae05f89cce4871ff827e91e
Dustin L. Howett <dustin@howett.net>
2022-12-08 17:21:55 -0600

    ALL PROJECTS TO 14.34

--- 140_release/vcrt_fwd_x64_release/vcruntime140_1_app/vcruntime140_1_app.cpp
+++ 140_release/vcrt_fwd_x64_release/vcruntime140_1_app/vcruntime140_1_app.cpp
@@ -1,1 +1,3 @@
 #pragma comment(linker, "/export:__CxxFrameHandler4=vcruntime140_1.__CxxFrameHandler4")
+#pragma comment(linker, "/export:__NLG_Dispatch2=vcruntime140_1.__NLG_Dispatch2")
+#pragma comment(linker, "/export:__NLG_Return2=vcruntime140_1.__NLG_Return2")
